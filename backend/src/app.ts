b5f39c334d6986e62898edc85a9e5c00a72d177c
afvr94 <abdiel017@gmail.com>
2022-01-28 13:45:59 -0500

    Added typescript to backend

---
+++ backend/src/app.ts
@@ -0,0 +1,39 @@
+import express, { Application, json, urlencoded } from 'express';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
+import path from 'path';
+
+dotenv.config();
+
+const app: Application = express();
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../../frontend/build')));
+
+app.use(helmet());
+
+// Handle GET requests to /api route
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', function (req, res) {
+  res.render('../../frontend/build/index.html');
+});
+
+try {
+  app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is listening to 👉🏼  http://localhost:${PORT}!`);
+  });
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.log('Error listing to server: ', err);
+}
