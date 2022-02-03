90d21978825bdadcbdc11c19bf7bb4afa36c4f4e
Abdiel Vega <abdiel017@gmail.com>
2022-02-03 13:08:43 -0500

    Added About page still needs some tweaking

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -30,6 +30,33 @@ const Spaceman = styled.div`
   }
 `;
 
+const Box = styled.div`
+  border: 2px solid ${(props) => props.theme.white};
+  color: ${(props) => props.theme.white};
+  padding: 2rem;
+  width: 50vw;
+  height: 60vh;
+  z-index: 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(0.6rem + 1vw);
+  backdrop-filter: blur(4px);
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 10rem;
+  overflow: auto;
+  p {
+    position: relative;
+    font-family: 'Ubuntu Mono', monospace;
+    font-style: italic;
+  }
+  & > p:not(:last-child) {
+    margin-bottom: 1.5rem;
+  }
+`;
+
 const About = () => {
   return (
     <ThemeProvider theme={theme}>
@@ -40,7 +67,22 @@ const About = () => {
         <Spaceman>
           <img src={astronaut} alt="Spaceman" />
         </Spaceman>
-        <div style={{ width: '' }}>TEST</div>
+        <Box>
+          <p>
+            I&apos;m a fullstack engineer and I graduated from the University of Puerto Rico in
+            Mayagüez with a degree in Computer Engineer. I love to create projects that makes an
+            impact to society.
+          </p>
+          <p>
+            I&apos;m interested in the whole frontend stack like React + Redux, etc. To build
+            amazing projects and bring designs to life. Also, I&apos;m interested in the Python +
+            NodeJS stacks for the backend.
+          </p>
+          <p>
+            I&apos;m a fitness freak, I like to weight lift and do cycling. I love to explore and
+            learn new topics in engineering.
+          </p>
+        </Box>
       </Container>
     </ThemeProvider>
   );
